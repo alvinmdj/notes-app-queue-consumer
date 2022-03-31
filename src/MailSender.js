cd2b9c6bfba3819d420fbefc13eea1b41667f4f0
Alvin <alvinmartindwrk@gmail.com>
2022-03-31 22:03:14 +0700

    create mail sender with nodemailer

---
+++ src/MailSender.js
@@ -0,0 +1,34 @@
+const nodemailer = require('nodemailer');
+
+class MailSender {
+  constructor() {
+    this._transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.MAIL_ADDRESS,
+        pass: process.env.MAIL_PASSWORD,
+      },
+    });
+  }
+
+  sendEmail(targetEmail, content) {
+    const message = {
+      from: 'Notes Apps',
+      to: targetEmail,
+      subject: 'Notes export',
+      text: 'Here is your exported notes',
+      attachment: [
+        {
+          filename: 'notes.json',
+          content,
+        },
+      ],
+    };
+
+    return this._transporter.sendMail(message);
+  }
+}
+
+module.exports = MailSender;
